b01596b46e56d381ece0dfe09310978e615cdcd7
Mário Monteiro <marioammonteiro@gmail.com>
2025-07-23 17:16:48 +0100

    finished POC

--- backup.sh
+++ backup.sh
@@ -6,7 +6,10 @@ FILE="${DIR}/${DT}.tgz"
 FILE_EXCLUDE="exclude.tag"
 mkdir ${DIR} -p
 touch .bak/${FILE_EXCLUDE}
+touch volumes/${FILE_EXCLUDE}
 touch target/${FILE_EXCLUDE}
+touch documents/${FILE_EXCLUDE}
+touch documents_not_used/${FILE_EXCLUDE}
 
 tar -zcvf ${FILE} \
 	--exclude-tag-all=${FILE_EXCLUDE} \
